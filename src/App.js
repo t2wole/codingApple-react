4fcfc7d2777a6e667562e6707e46f24c03ef6231
t2wole <t2wole@gmail.com>
2022-04-05 17:30:04 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목1, 글제목변경1] = useState('남자 코트 추천');
+  let [글제목2, 글제목변경2] = useState('여자 코트 추천');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          하이!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>개발 Blog</div>
+      </div>
+      <div className='list'>
+        <h3>{ 글제목1 }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{ 글제목2 }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
